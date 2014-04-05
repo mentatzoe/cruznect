6c6eccaacf2d1b889c9bd77bb7a9dda8a0092fe0
DeiSu <deisu.tech@gmail.com>
2014-04-05 14:38:47 -0700

    Converted LoginViewController into LoginTVC

---
+++ Cruznect/Cruznect/LoginTVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginTVC.h
+//  Cruznect
+//
+//  Created by Bruce•D•Su on 4/5/14.
+//  Copyright (c) 2014 Cruznect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginTVC : UITableViewController
+
+@end
---
+++ Cruznect/Cruznect/LoginTVC.m
@@ -0,0 +1,93 @@
+//
+//  LoginTVC.m
+//  Cruznect
+//
+//  Created by Bruce•D•Su on 4/5/14.
+//  Copyright (c) 2014 Cruznect. All rights reserved.
+//
+
+#import "LoginTVC.h"
+
+@interface LoginTVC ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (strong, nonatomic) UIAlertView *loginErrorAlertView;
+@end
+
+@implementation LoginTVC
+
+NSString * const kLoginScriptURLString = @"";
+
+- (BOOL)executeRequestWithRequestBody:(NSString *)requestBody
+{
+    NSURL *reqeustURL = [NSURL URLWithString:kLoginScriptURLString];
+    const char *requestBodyStr = [requestBody UTF8String];
+    NSData *requestData = [NSData dataWithBytes:requestBodyStr length:strlen(requestBodyStr)];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:reqeustURL];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:requestData];
+    
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:NULL error:NULL];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+	
+    NSDictionary *results = data ? [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers|NSJSONReadingMutableLeaves error:nil] : nil;
+    
+    if ([[results objectForKey:@"status-code"] isEqual:[NSNumber numberWithInt:200]]) {
+        [defaults setObject:@"YES" forKey:@"login"];
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+NSString * const kLoginErrorAlertTitle = @"Login Error";
+NSString * const kLoginErrorAlertMessage = @"Check you username and password";
+
+- (UIAlertView *)loginErrorAlertView
+{
+    if (!_loginErrorAlertView) {
+        _loginErrorAlertView = [[UIAlertView alloc] initWithTitle:kLoginErrorAlertTitle
+														  message:kLoginErrorAlertMessage
+														 delegate:self
+												cancelButtonTitle:@"Done"
+												otherButtonTitles:nil];
+    }
+    return _loginErrorAlertView;
+}
+
+- (IBAction)loginButtonPressed:(id)sender
+{
+	NSString *usernameString = self.usernameTextField.text;
+	NSString *passwordString = self.passwordTextField.text;
+	
+	NSString *requestBody = [NSString stringWithFormat:@"username=%@&password=%@", usernameString, passwordString];
+    
+	UIActivityIndicatorView *spinner = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    [spinner startAnimating];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:spinner];
+    
+    dispatch_queue_t verifyQ = dispatch_queue_create("Cruznect Verify", NULL);
+    dispatch_async(verifyQ, ^{
+        BOOL login = [self executeRequestWithRequestBody:requestBody];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (login) {
+                self.navigationItem.rightBarButtonItem = nil;
+				
+                // Save username and password
+				NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+				[defaults setObject:usernameString forKey:@"username"];
+				[defaults setObject:passwordString forKey:@"password"];
+				
+				// Perform Segue
+				[self performSegueWithIdentifier:@"Login" sender:self];
+            } else {
+				self.navigationItem.rightBarButtonItem = sender;
+                [self.loginErrorAlertView show];
+            }
+        });
+    });
+}
+
+@end

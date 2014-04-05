cdc757b1dbb1f7c58352ec474b7fa06bdc4cab29
DeiSu <deisu.tech@gmail.com>
2014-04-05 12:36:40 -0700

    Created Cruznect iOS project

---
+++ Cruznect/Cruznect/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Cruznect
+//
+//  Created by Bruce•D•Su on 4/5/14.
+//  Copyright (c) 2014 Cruznect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end

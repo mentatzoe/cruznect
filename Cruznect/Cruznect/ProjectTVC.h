b69a1725c6568c052b7a190392ef980557599fe6
DeiSu <deisu.tech@gmail.com>
2014-04-05 22:01:30 -0700

    Added ProjectTVC

---
+++ Cruznect/Cruznect/ProjectTVC.h
@@ -0,0 +1,13 @@
+//
+//  ProjectTVC.h
+//  Cruznect
+//
+//  Created by Bruce•D•Su on 4/5/14.
+//  Copyright (c) 2014 Cruznect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectTVC : UITableViewController
+
+@end

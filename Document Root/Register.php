5b9d2a74def6c3bd5f5d531378159601aba35b79
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-04-05 20:25:12 -0700

    Added images folder and icons

--- Document Root/Register.php
+++ Document Root/Register.php
@@ -1,6 +1,21 @@
 <?php
 	include 'functions.php';
-
+	echo "hello";
+	var_dump($_POST);
+	
+	echo count($_POST['talents']);
+	if(!empty($_POST['talents'])) {
+    	
+	}
+	
+if(!empty($_POST['talents'])) {
+    foreach($_POST['talents'] as $check) {
+            echo $check; //echoes the value set in the HTML form for each checked checkbox.
+                         //so, if I were to check 1, 3, and 5 it would echo value 1, value 3, value 5.
+                         //in your case, it would echo whatever $row['Report ID'] is equivalent to.
+    }
+}
+	
 	$username=$_POST["username"];
 	$password=$_POST["password"];
 

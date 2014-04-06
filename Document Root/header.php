fe9df36c29fb184445ec664d35651097b992b738
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-04-06 15:19:08 -0700

    Last minute stuff

--- Document Root/header.php
+++ Document Root/header.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <!--Name and meta tags-->
-
+	<title>Project Slug</title>
 <!--Including external jQuery-->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
 <!--Link to CSS-->

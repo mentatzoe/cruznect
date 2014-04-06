bbaf6a742b26ffcb8d4b763278853029e6576124
Maria Lopez-Latorre <mentatzoe@gmail.com>
2014-04-05 22:57:44 -0700

    Revert "Revert 85b949a..5831319"
    This reverts commit 8308ad449e1f5d4fb1c1f32fc2793c54770cbe8a.

--- Document Root/header.php
+++ Document Root/header.php
@@ -6,6 +6,10 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
 <!--Link to CSS-->
 <link rel="stylesheet" type="text/css" href="style.css">
+<? include "functions.php"; 
+	session_start();
+	$_SESSION['email'] = "asd";
+?>
 </head>
 
 <body>
